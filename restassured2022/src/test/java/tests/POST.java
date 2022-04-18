7ba3d8d85d95f0af5eb080eafdfce5fb1a58ccbd
Chandra Shekhar pandey <cspandey3000@gmail.com>
2022-04-18 07:28:42 +0530

    18/04/2033 - write resrapi and read and verify output

--- restassured2022/src/test/java/tests/POST.java
+++ restassured2022/src/test/java/tests/POST.java
@@ -1,50 +1,88 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import model.Booking;
-import model.Bookingdates;
 import model.completeJSON;
 import model.employeeData;
 
 public class POST extends BaseTest {
 
-  private static final String jsonMockURL = "https://run.mocky.io/v3/f8d0c78c-d7a3-496c-8245-6df6a5327eb1";
-    @Test
-    public void createBooking() {
-        String response =RestAssured
-                .given() // caaled from basetest
-                // .spec(reqspec)
+  // private static final String jsonMockURL =
+  // "https://run.mocky.io/v3/f8d0c78c-d7a3-496c-8245-6df6a5327eb1";
+  // private static employeeData obj;
 
-                .contentType(ContentType.JSON)
-              .body(completeJSON.getData())//serialize java object to json
+  @Test(enabled = false)
+  public void createBooking() throws JsonMappingException, JsonProcessingException {
 
-                .when()
-                .post()
+    ObjectMapper mapper = new ObjectMapper();
 
-                // validate response
-                .then().extract().toString();
-                /* .log()
-                .all(); */
+    String jsonsObject = mapper.writeValueAsString(RestAssured
+        .given() // called from basetest
+        // .spec(reqspec)
 
-                System.out.println(response);
+        .contentType(ContentType.JSON)
+        .body(completeJSON.getData())// serialize java object to json
 
+        .when()
+        .post().as(employeeData.class));
+
+    System.out.println(jsonsObject);
+
+    // .then()
+    /*
+     * .log()
+     * .all();
+     */
+
+    // validate response
+    // .then();
+
+  }
+
+  @Test(priority = 2)
+  public void createAndVerifyBooking() throws JsonMappingException, JsonProcessingException {
+
+    ObjectMapper mapper = null;
+    final String path = System.getProperty("user.dir") + "/src/test/java/resources/booking.json";
+    employeeData jsonsObject = RestAssured
+        .given() // called from basetest
+        // .spec(reqspec)
+
+        .contentType(ContentType.JSON)
+        .body(completeJSON.getData())// serialize java object to json
+
+        .when()
+        .post().as(employeeData.class);
+
+    // write object to file
+
+    try {
+      mapper = new ObjectMapper();
+
+      mapper.writeValue(new File(path), jsonsObject);
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    @Test
-    public void verifyBooking() {
-        
-      //TO-DO :instead of jsonMOCKURL we can pass created response saved in json file
-      employeeData obj = RestAssured.get(jsonMockURL).as(employeeData.class);
-      System.out.println(obj.getBookingid());
 
-      Booking bobj = obj.getBooking();
-      System.out.println(bobj.getFirstname());
-
-      Bookingdates bkngdates = bobj.getBookingdates();
-      System.out.println(bkngdates.getCheckin());
-
+    // read value from jsonfile-mapped with POJO
+    try {
+      employeeData empdata = mapper.readValue(new File(path), employeeData.class);
+      System.out.println(empdata.getBookingid());
+      System.out.println(empdata.getBooking().getFirstname());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+  }
 
 }
